da849c4f0e8bc9a140636756d35dccac5413623e
Ilia Teplischchev <ilia.teplishchev@vertexbreak.com>
2026-07-20 17:11:36 +0400

    Create dynamite object in the main thread

--- hook/src/main.cpp
+++ hook/src/main.cpp
@@ -11,7 +11,8 @@ extern HMODULE origDll; // dinputproxy
 #include "DynamiteHook.h"
 
 DWORD WINAPI InitThread(LPVOID lpParameter) {
-    new Dynamite::Dynamite;
+    Sleep(1000);
+    ((Dynamite::Dynamite *)lpParameter)->CreateD3DHook();
 
     return 0;
 } // InitThread
@@ -21,10 +22,11 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hModule);
         g_thisModule = hModule;
+        auto dynamite = new Dynamite::Dynamite();
 
-        HANDLE hInitThread = CreateThread(nullptr, 0, InitThread, hModule, 0, nullptr);
+        HANDLE hInitThread = CreateThread(nullptr, 0, InitThread, dynamite, 0, nullptr);
         if (hInitThread == NULL) {
-
+        
         } else {
             CloseHandle(hInitThread);
         }
